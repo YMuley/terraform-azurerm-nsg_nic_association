da25d029fdef0a3cba49c30505af2034cf507f2c
sunandasharma61 <sunandasharma60101@gmail.com>
2023-11-03 17:08:47 +0530

    testing

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 #----------- Network Security Group - Network Interface Association
 resource "azurerm_network_interface_security_group_association" "nic_nsg_association" {
   for_each                  = local.network_security_group
-  network_interface_id      = var.network_interface_card_output[each.key].id
+  network_interface_id      = var.network_interface_card_output[each.value.nic_id].id
   network_security_group_id = var.network_security_group_output[each.value.nsg_name].id
 }
