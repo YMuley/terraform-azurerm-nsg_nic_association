a7437c2f44832a92f2e46ec07f2cb3769d7a430f
sunandasharma61 <sunandasharma60101@gmail.com>
2023-11-03 16:52:44 +0530

    testing

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "network_security_group_network_interface_card_association_output" {
-  value       = azurerm_network_security_group_network_interface_association.nic_nsg_association
+  value       = azurerm_network_interface_security_group_association.nic_nsg_association
   description = "Outputs of nsg and nic association object"
 }
 
