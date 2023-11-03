a1c264e845f222eea5ba6f0a8b278c118e23bd97
sunandasharma61 <sunandasharma60101@gmail.com>
2023-11-03 16:42:39 +0530

    testing

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 #----------- Network Security Group - Network Interface Association
-resource "azurerm_network_security_group_network_interface_association" "network_security_group_network_interface_association" {
-  for_each                         = local.network_security_group
-  network_interface_association_id = var.network_interface_output[each.value.network_interface_id].id
-  network_security_group_id        = var.network_security_group_output[each.value.nsg_name].id
+resource "azurerm_network_security_group_network_interface_association" "nic_nsg_association" {
+  for_each                  = local.network_security_group
+  network_interface_id      = var.network_interface_card_output[each.value.network_interface_card_id].id
+  network_security_group_id = var.network_security_group_output[each.value.nsg_name].id
 }
--- output.tf
+++ output.tf
@@ -1,6 +1,5 @@
-output "network_security_group_network_interface_association_output" {
-  value       = azurerm_network_security_group_network_interface_association.network_security_group_network_interface_association
+output "network_security_group_network_interface_card_association_output" {
+  value       = azurerm_network_security_group_network_interface_association.nic_nsg_association
   description = "Outputs of nsg and nic association object"
 }
-
 
